c79eb0d7b568bc0904fc7848c27a81434a069ce6
Ramesh Yadav <rameshyadavjee@gmail.com>
2022-01-22 08:00:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-  <BrowserRouter basename="reactjs">
+  <BrowserRouter basename="reactjs/build">
     <App />
 	</BrowserRouter>
   </React.StrictMode>,
