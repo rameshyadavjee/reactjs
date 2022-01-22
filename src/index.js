6172ac960fc05226ef863e2393264083b7c95d10
Ramesh Yadav <rameshyadavjee@gmail.com>
2022-01-22 08:47:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
  
 ReactDOM.render(
    
     <App />
    
-  document.getElementById("root");
+  document.getElementById("root")
 );
  
 
