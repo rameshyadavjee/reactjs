b0714056e12908367b40c1d4977b89113716b297
Ramesh Yadav <rameshyadavjee@gmail.com>
2022-01-22 08:32:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
  
 ReactDOM.render(
-  <React.StrictMode>   
-    <App />	 
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter basename="reactjs/build">
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
+
+Route Matchers
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
